b794da5a5b394d991763795fa8f8f22d1c6f08bf
ochiroo0421 <148938538+ochiroo0421@users.noreply.github.com>
2026-06-04 18:44:57 +0800

    Add testimonials page

---
+++ src/app/[locale]/testimonials/page.tsx
@@ -0,0 +1,9 @@
+import TestimonialsSection from "@/components/TestimonialsSection";
+
+export default function TestimonialsPage() {
+  return (
+    <div className="pt-20">
+      <TestimonialsSection />
+    </div>
+  );
+}
