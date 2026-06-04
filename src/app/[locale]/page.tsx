25cc59388060cd38f77bcb5cbe88828cb77d9046
ochiroo0421 <148938538+ochiroo0421@users.noreply.github.com>
2026-06-04 18:44:10 +0800

    Add homepage

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,15 @@
+import HeroSection from "@/components/HeroSection";
+import StatsSection from "@/components/StatsSection";
+import TestimonialsSection from "@/components/TestimonialsSection";
+import ContactSection from "@/components/ContactSection";
+
+export default function HomePage() {
+  return (
+    <>
+      <HeroSection />
+      <StatsSection />
+      <TestimonialsSection />
+      <ContactSection />
+    </>
+  );
+}
