b74cb6686a1364361c32451621ceaf4ca1470a87
ochiroo0421 <148938538+ochiroo0421@users.noreply.github.com>
2026-06-04 19:22:44 +0800

    Update homepage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,11 +5,11 @@ import ContactSection from "@/components/ContactSection";
 
 export default function HomePage() {
   return (
-    <>
+    <div className="bg-black">
       <HeroSection />
       <StatsSection />
       <TestimonialsSection />
       <ContactSection />
-    </>
+    </div>
   );
 }
