8f4dcdb5e2fd7883e82f19a88473546b488bc137
ochiroo0421 <148938538+ochiroo0421@users.noreply.github.com>
2026-06-04 19:22:47 +0800

    Update testimonials page

--- src/app/[locale]/testimonials/page.tsx
+++ src/app/[locale]/testimonials/page.tsx
@@ -2,7 +2,7 @@ import TestimonialsSection from "@/components/TestimonialsSection";
 
 export default function TestimonialsPage() {
   return (
-    <div className="pt-20">
+    <div className="pt-20 bg-black min-h-screen">
       <TestimonialsSection />
     </div>
   );
