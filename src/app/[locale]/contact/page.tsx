d7f850306496e63e0d1e610f54978f299c5b10f5
ochiroo0421 <148938538+ochiroo0421@users.noreply.github.com>
2026-06-04 19:22:50 +0800

    Update contact page

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -2,7 +2,7 @@ import ContactSection from "@/components/ContactSection";
 
 export default function ContactPage() {
   return (
-    <div className="pt-20">
+    <div className="pt-20 bg-black min-h-screen">
       <ContactSection />
     </div>
   );
