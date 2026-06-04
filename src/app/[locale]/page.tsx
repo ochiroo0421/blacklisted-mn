0ee25e9d3e16485f0767b5cca7f7241bb82465e9
ochiroo0421 <148938538+ochiroo0421@users.noreply.github.com>
2026-06-04 19:40:51 +0800

    Add FeaturesSection and SearchSection to homepage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,16 @@
 import HeroSection from "@/components/HeroSection";
+import SearchSection from "@/components/SearchSection";
+import FeaturesSection from "@/components/FeaturesSection";
 import StatsSection from "@/components/StatsSection";
 import TestimonialsSection from "@/components/TestimonialsSection";
 import ContactSection from "@/components/ContactSection";
 
 export default function HomePage() {
   return (
-    <div className="bg-black">
+    <div className="bg-[#0A0C12]">
       <HeroSection />
+      <SearchSection />
+      <FeaturesSection />
       <StatsSection />
       <TestimonialsSection />
       <ContactSection />
